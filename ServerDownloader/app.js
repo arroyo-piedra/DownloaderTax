2548f38252b8ef417d53f4244f6e3c682993a66b
Charlie83 <carlosgomezsoza@gmail.com>
2018-10-04 14:00:46 -0600

    actualizacion codigo de andre

--- ServerDownloader/app.js
+++ ServerDownloader/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var tree = require('./conectToMongo');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.get('/search', function(req, res) {
+  //res.send('ab?cd');
+  let tax = req.query.tax;
+  let name = req.query.name;
+  if(name !== undefined && tax !== undefined){
+  	tree.buildTreeTax(tax,name,res);
+   }
+
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +50,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
